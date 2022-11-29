d7dd4941a95fa8a7a606ded41b022c810b14df91
Ade Setiawan <adesetiawan0675@gmail.com>
2022-11-29 20:03:07 +0700

    edit paginate

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Pagination = () => {
     return (
-        <div className="mb-20 mt-5 flex flex-col items-center">
+        <div className="mb-20 mt-10 flex flex-col items-center">
             <div className="flex text-desc">
-                <div className="mr-1 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-gray-200">
+                <div className="mr-1 flex h-11 w-11 cursor-pointer items-center justify-center rounded-lg bg-slate-100">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -20,33 +20,24 @@ const Pagination = () => {
                         />
                     </svg>
                 </div>
-                <div className="flex h-12 rounded-full bg-white font-medium">
-                    <div className="hidden w-12 cursor-pointer items-center justify-center  rounded-full text-sm leading-5 transition duration-150 ease-in md:flex">
+                <div className="flex h-11 rounded-lg bg-white font-medium">
+                    <div className="hidden w-11 cursor-pointer items-center justify-center  rounded-lg text-sm leading-5 transition duration-150 ease-in md:flex">
                         1
                     </div>
-                    <div className="hidden w-12 cursor-pointer items-center justify-center  rounded-full bg-primary leading-5 text-white transition duration-150 ease-in md:flex ">
+                    <div className="hidden w-11 cursor-pointer items-center justify-center  rounded-lg bg-primary leading-5 text-white transition duration-150 ease-in md:flex ">
                         2
                     </div>
-                    <div className="hidden w-12 cursor-pointer items-center justify-center  rounded-full text-sm leading-5 transition duration-150 ease-in md:flex">
+                    <div className="hidden w-11 cursor-pointer items-center justify-center  rounded-lg text-sm leading-5 transition duration-150 ease-in md:flex">
                         3
                     </div>
-                    <div className="hidden w-12 cursor-pointer items-center justify-center  rounded-full text-sm leading-5 transition duration-150 ease-in md:flex">
+                    <div className="hidden w-11 cursor-pointer items-center justify-center  rounded-lg text-sm leading-5 transition duration-150 ease-in md:flex">
                         ...
                     </div>
-                    <div className="hidden w-12 cursor-pointer items-center justify-center  rounded-full text-sm leading-5 transition duration-150 ease-in md:flex">
-                        13
-                    </div>
-                    <div className="hidden w-12 cursor-pointer items-center justify-center  rounded-full text-sm leading-5 transition duration-150 ease-in md:flex">
-                        14
-                    </div>
-                    <div className="hidden w-12 cursor-pointer items-center justify-center  rounded-full text-sm leading-5 transition duration-150 ease-in md:flex">
-                        15
-                    </div>
-                    <div className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-primary leading-5 text-white transition duration-150 ease-in md:hidden">
-                        2
+                    <div className="hidden w-11 cursor-pointer items-center justify-center  rounded-lg text-sm leading-5 transition duration-150 ease-in md:flex">
+                        25
                     </div>
                 </div>
-                <div className="ml-1 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-gray-200">
+                <div className="ml-1 flex h-11 w-11 cursor-pointer items-center justify-center rounded-lg bg-slate-100">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
